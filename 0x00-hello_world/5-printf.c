2beaf1bd0e194abd5619ca689a7a00a590a077cf
Brainbox27 <oluwatobigbolahan@gmail.com>
2022-09-08 18:30:18 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the printf function
+ *
+ * Description:using the main function 
+ * this code will print with proper grammar, but the outcome is a piece of art, on a new line
+ * Return: 0
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
