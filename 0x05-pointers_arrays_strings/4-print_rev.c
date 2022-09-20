ef73f0d3b1e6d93b7d68534e62e7585e5f367c2f
Brainbox27 <oluwatobigbolahan@gmail.com>
2022-09-19 18:08:41 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,31 +1,31 @@
 #include "main.h"
 /**
- * * print_rev - prints a string in reverse
- * * @s: string to print
- * */
+* print_rev - prints a string in reverse
+* @s: string to print
+*/
 
 void print_rev(char *s)
 {
-	int len = 0, index = 0;
-	len = _strlen(s);
+int len = 0, index = 0;
+len = _strlen(s);
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+for (index = len - 1; index >= 0; index--)
+_putchar(s[index]);
 
-	_putchar('\n');
+_putchar('\n');
 }
 
 /**
- *  *_strlen - returns the length of a string
- *   * @s: string
- *    *Return: returns lenght;
- *     */
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
 int _strlen(char *s)
 {
-	int count, inc;
-	inc = 0;
-	for (count = 0; s[count] != '\0'; count++)
-		inc++;
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
 
-	return (inc);
-}
+return (inc);
+
